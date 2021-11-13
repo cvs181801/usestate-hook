b31b1aa7de8d8dfb67b84656b31ebc8cda93bb4a
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-11-12 16:06:37 -0800

    add useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import Randomcolor from 'randomcolor'
 
 function App() {
   const [number, setNumber] = useState(0)
   const [word, setWord] = useState("WOW")
+  //var color = randomColor();
+  const [randomcolor, setRandomcolor] = useState('')
+  
+
+   useEffect(() => {
+    setRandomcolor(randomColor())
+  }, [number])
 
   return (
     <div className="App">
       <h1>{number}</h1>
       <button
+        style={{color: randomcolor}}
         onClick={()=>setNumber(prevNumber => prevNumber+1)} //using the ++ operator didn't work here FYI
       >
         Count Up!
